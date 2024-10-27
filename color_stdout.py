dba99b82f25e3b0a9bbfa5ffa35babd110b308c3
Tastror <yccozy2001@163.com>
2024-10-27 13:35:30 +0800

    add str',', int, list & tuple for color; add more names in ANSI

--- color_stdout.py
+++ color_stdout.py
@@ -14,23 +14,61 @@ usual_input = input
 # CSI E (Next Line), CSI F (Previous Line), CSI S (Next Page), CSI T (Previous Page)
 
 
-def color_to_code(color: str) -> str:
+def color_to_code(
+    color: str | int | list[str|int] | tuple[str|int, ...]
+) -> str:
+
+    if type(color) == int:
+        color_list = [color]
+    elif type(color) == str:
+        color_list = color.lower().split(',')
+        color_list = [i.strip() for i in color_list]
+    elif type(color) == list or type(color) == tuple:
+        color_list = color
+        color_list = [i.strip() if type(i) == str else i for i in color_list]
+    else:
+        return ""
+    if len(color_list) == 0:
+        return ""
+
     # \033[xm: change to x color (0 reset, 1 bold, 4 underline, 30~37 90~97 colors)
     # \033[a;b;cm: equal to \033[am\033[bm\033[cm
-    reset, bold, _, _, underline = tuple(f"\033[{i}m" for i in range(0, 4 + 1))
-    dark_black, dark_red, dark_green, dark_yellow, dark_blue, dark_purple, dark_cyan, dark_white = tuple(f"\033[{i}m" for i in range(30, 37 + 1))
-    black, red, green, yellow, blue, purple, cyan, white = tuple(f"\033[{i}m" for i in range(90, 97 + 1))
-    del _
-    for name, code in locals().items():
-        if name == color:
-            return code
+    # faint, italic, blink, fast_blink is not supported in most terminals
+    reset, bold, faint, italic, underline, blink, fast_blink = tuple(i for i in range(0, 6 + 1))
+    dark_black, dark_red, dark_green, dark_yellow, dark_blue, dark_purple, dark_cyan, dark_white = \
+        tuple(i for i in range(30, 37 + 1))
+    bg_dark_black, bg_dark_red, bg_dark_green, bg_dark_yellow, bg_dark_blue, bg_dark_purple, bg_dark_cyan, bg_dark_white = \
+        tuple(i for i in range(40, 47 + 1))
+    black, red, green, yellow, blue, purple, cyan, white = \
+        tuple(i for i in range(90, 97 + 1))
+    bg_black, bg_red, bg_green, bg_yellow, bg_blue, bg_purple, bg_cyan, bg_white = \
+        tuple(i for i in range(100, 107 + 1))
+
+    result_code = "\033["
+    result_code_end = "m"
+    for i in color_list:
+        if type(i) == str:
+            if i in locals():
+                result_code += str(locals()[i]) + ";"
+            elif i.isdigit():
+                result_code += i + ";"
+        elif type(i) == int:
+            result_code += str(i) + ";"
+        else:
+            pass
+    if result_code[-1] == ";":
+        result_code = result_code[:-1]
+    return result_code + result_code_end
 
 def format_color(
     text: object,
-    color: str | None = None,
+    color: str | int | list[str|int] | tuple[str|int, ...] | None = None,
     *,
-    end_reset: bool = True
+    end_reset: bool = True  # only works when color is not None
 ) -> str:
+    """
+    end_reset: only works when color is not None
+    """
     if color is None:
         return f"{text}"
     elif end_reset:
@@ -42,11 +80,14 @@ def print(
     *values: object,
     sep: str | None = " ",
     end: str | None = "\n",
-    color: str | None = None,
+    color: str | int | list[str|int] | tuple[str|int, ...] | None = None,
     end_reset: bool = True,  # only works when color is not None
     # file: SupportsWrite[str] | None = None,  # important, always sys.stdout
     flush: bool = True  # important, different from usual print
 ) -> None:
+    """
+    end_reset: only works when color is not None
+    """
     if color is None:
         usual_print(*values, sep=sep, end=end)
     else:
@@ -61,12 +102,15 @@ def input(
     *prompts: object,
     sep: str | None = " ",
     end: str | None = "",  # important
-    color: str | None = None,
-    input_color: str | None = None,
+    color: str | int | list[str|int] | tuple[str|int, ...] | None = None,
+    input_color: str | int | list[str|int] | tuple[str|int, ...] | None = None,
     end_reset: bool = True,  # only works when input_color is not None
     # file: SupportsWrite[str] | None = None,  # important, always sys.stdout
     flush: bool = True  # important
 ) -> str:
+    """
+    end_reset: only works when color is not None
+    """
     print(*prompts, sep=sep, end=end, color=color, end_reset=True, flush=flush)
     if input_color is None:
         data = usual_input()
@@ -196,10 +240,13 @@ def print_there(
     column: int | None = None,
     sep: str | None = " ",
     end: str | None = "",  # important
-    color: str | None = None,
+    color: str | int | list[str|int] | tuple[str|int, ...] | None = None,
     end_reset: bool = True,
     flush: bool = True
 ):
+    """
+    end_reset: only works when color is not None
+    """
     move(line, column, flush=False)
     print(*value, sep=sep, end=end, color=color, end_reset=end_reset, flush=flush)
 
@@ -207,10 +254,13 @@ def print_still(
     *value: object,
     sep: str | None = " ",
     end: str | None = "",  # important
-    color: str | None = None,
+    color: str | int | list[str|int] | tuple[str|int, ...] | None = None,
     end_reset: bool = True,
     flush: bool = True
 ):
+    """
+    end_reset: only works when color is not None
+    """
     store_cursor(flush=False)
     print(*value, sep=sep, end=end, color=color, end_reset=end_reset, flush=False)
     restore_cursor(flush=flush)
@@ -219,13 +269,18 @@ def print_thisline(
     *value: object,
     sep: str | None = " ",
     end: str | None = "",  # important
-    color: str | None = None,
+    color: str | int | list[str|int] | tuple[str|int, ...] | None = None,
     end_reset: bool = True,
     flush: bool = True
 ):
+    """
+    end_reset: only works when color is not None
+    """
     clean_line_and_to_start(flush=False)
     print(*value, sep=sep, end=end, color=color, end_reset=end_reset, flush=flush)
 
+def color_reset(*, flush: bool = True):
+    print(color="reset", end_reset=False, end="", flush=flush)
 
 
 if __name__ == "__main__":
@@ -235,12 +290,17 @@ if __name__ == "__main__":
     clean_and_to_start()
 
     move(2, 1)
-    print(11111111)
-    print(22222222)
-    print(33333333)
+    print(11111111, color="red")
+    # str with "," will be split
+    print(22222222, color="bg_dark_red, dark_green")
+    # list is also supported, but no "," in str inner list
+    print(33333333, color=("italic", "underline", "bold", "blink"), end_reset=False)
+    # thongh 44444444's end_reset == True, it won't work since no color specified
     print(44444444)
+    # use this for reset, or color="reset" in next print
+    color_reset()
     print(55555555)
-    print_there(66666666, line=1, column=9)
+    print_there(66666666, line=1, column=9, color="yellow, fast_blink")
     print_there(77777777, line=4)
     print_there(88888888, line=7, column=1)
     print_there(99999999, column=10)
@@ -248,15 +308,17 @@ if __name__ == "__main__":
 
     move_down(2)
     move_left(1000)
-    input("normal", "color:", sep=" ", end=" ")  # use print-like *value, sep and end
-    input("red prompt: ", color="red")
-    input("green input: ", input_color="green")
-    input("red prompt, green input: ", color="red", input_color="green")
+    # input can use print-like *value, sep and end for better use
+    input(1, "normal", "color:", sep=" ", end=" ")
+    input(2, "red prompt: ", color="red, italic")
+    input(3, "green input: ", color="italic", input_color="green, underline")
+    input(4, "red prompt, green input: ", color="red, italic", input_color="green, underline")
 
     import time
     move_right(10)
-    for i in range(10):
-        print_still(i, end="")
+    for i in range(8):
+        # use number (or list contains number) for color is also supported
+        print_still(100000 + i, end="", color=i + 30)
         time.sleep(0.2)
     print(12312321312123213, end="")
     time.sleep(0.2)
